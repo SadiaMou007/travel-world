44fcb0066e88764d6d9f4cf7d3bedb56366f5175
SadiaMou007 <60190292+SadiaMou007@users.noreply.github.com>
2022-03-01 06:27:10 -0800

    complete project upload

---
+++ src/components/HeroSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "./Button";
+
+import "../App.css";
+import "./HeroSection.css";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <h1>Go Endlessly</h1>
+      <p>Ready to explore?</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Get Started
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          Travel YouTube <i className="far fa-play-circle" />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
